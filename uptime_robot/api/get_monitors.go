509ff68cedd9b39905e37113b728df4b751633dc
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:36 -0500

    Add struct for getMonitor query parameters

--- uptime_robot/api/get_monitors.go
+++ uptime_robot/api/get_monitors.go
@@ -1,8 +1,14 @@
 package api
 
-import "github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot/models"
+import (
+	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot/models"
+)
 
 type GetMonitors struct {
 	Envelope
 	Monitors []models.Monitor `json:"monitors,omitempty"`
+}
+
+type GetMonitorsQuery struct {
+	Monitors []models.Monitor `schema:"monitors,omitempty"`
 }
