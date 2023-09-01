b6b0099bd6c83e91c070589c451ec883f24230fa
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:35 -0500

    Add template for API requests

--- uptime_robot/client.go
+++ uptime_robot/client.go
@@ -2,6 +2,12 @@ package uptime_robot
 
 import (
 	"net/http"
+	"strings"
+)
+
+const (
+	baseUrl = "https://api.uptimerobot.com/v2/"
+	userAgent = "go-uptimerobot-v2/1.0.0 (Language=Go)"
 )
 
 type Client struct {
@@ -10,4 +16,20 @@ type Client struct {
 
 func New() *Client {
 	return &Client{HttpClient: http.DefaultClient}
+}
+
+func NewRequest(apiMethod string) (*http.Request, error) {
+	apiUrl := baseUrl + apiMethod
+
+	encodedForm := strings.NewReader("")
+
+	req, err := http.NewRequest(http.MethodPost, apiUrl, encodedForm)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	return req, nil
 }
