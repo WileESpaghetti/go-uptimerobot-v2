faea8ad527b4972932792cd383b5ae81c60695fc
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:36 -0500

    use cobra-cli to scaffold command-line client

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # go-uptimemonitor-v2
-Uptime Robot APIv2 command line client and library
+Uptime Robot APIv2 command-line client and library
 
 [UptimeRobot API Documentation](https://uptimerobot.com/api)
+
+
+## Build
+```shell
+go build -o uptimerobot cmd/uptimerobot/main.go
+```
--- cmd/uptimerobot/main.go
+++ cmd/uptimerobot/main.go
@@ -1,31 +1,11 @@
 package main
 
-import (
-	"context"
-	"flag"
-	"github.com/WileESpaghetti/go-uptimerobot-v2/cmd/uptimerobot/commands"
-	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot"
-	"github.com/google/subcommands"
-	"os"
-)
+import "github.com/WileESpaghetti/go-uptimerobot-v2/cmd/uptimerobot/cmd"
 
+// TODO handle signals
+// TODO add Context
+// TODO don't use hard-coded root command name (example: see bash script best practices)
+// TODO consider matching how gcloud and kubectl commands work. ($cmd [verb] [resource]) vs how we do it ($cmd [resource] [verb]). would also want to flip the command/file name if we do that
 func main() {
-	var apiKeyFlag string
-
-	flag.StringVar(&apiKeyFlag, "api-key", "", "Account or Monitor Specific API key")
-
-	client := uptime_robot.New("")
-
-	subcommands.Register(subcommands.HelpCommand(), "")
-	subcommands.Register(subcommands.FlagsCommand(), "")
-	subcommands.Register(subcommands.CommandsCommand(), "")
-	subcommands.Register(&commands.Account{Client: client}, "")
-	subcommands.Register(&commands.Monitors{Client: client}, "")
-
-	flag.Parse()
-
-	client.ApiKey = apiKeyFlag
-
-	ctx := context.Background()
-	os.Exit(int(subcommands.Execute(ctx)))
+	cmd.Execute()
 }
