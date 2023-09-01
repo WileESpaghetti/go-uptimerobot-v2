5d9b85e13d59040ba0146788deb2fbc91fdf751b
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:35 -0500

    Convert account details to a string

--- uptime_robot/models/account.go
+++ uptime_robot/models/account.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Account struct {
 	DownMonitors    int64  `json:"down_monitors"    xml:"down_monitors,attr"`
 	Email           string `json:"email"            xml:"email,attr"`
@@ -9,3 +11,21 @@ type Account struct {
 	UpMonitors      int64  `json:"up_monitors"      xml:"up_monitors,attr"`
 }
 
+func (a Account) String() string {
+	accountFormat := "Account Details:" +
+		"\n Email:            %s" +
+		"\n Monitor Limit:    %d" +
+		"\n Monitor Interval: %d minute(s)" +
+		"\n\nMonitor Details:" +
+		"\n Up:     %d" +
+		"\n Down:   %d" +
+		"\n Paused: %d\n"
+
+	return fmt.Sprintf(accountFormat,
+		a.Email,
+		a.MonitorLimit,
+		a.MonitorInterval,
+		a.UpMonitors,
+		a.DownMonitors,
+		a.PausedMonitors)
+}
