a2394eaa4d79f26302f2e65673bbc95d719f19a1
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:36 -0500

    Add custom encoder for getMonitorQuery to gorilla schema encoder

--- uptime_robot/api/get_monitors.go
+++ uptime_robot/api/get_monitors.go
@@ -1,7 +1,12 @@
 package api
 
 import (
+	"fmt"
 	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot/models"
+	"github.com/gorilla/schema"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 type GetMonitors struct {
@@ -11,4 +16,24 @@ type GetMonitors struct {
 
 type GetMonitorsQuery struct {
 	Monitors []models.Monitor `schema:"monitors,omitempty"`
+}
+
+func (m GetMonitorsQuery) RegisterEncoders(e *schema.Encoder) {
+	e.RegisterEncoder(m.Monitors, MonitorsSchemaEncoder)
+}
+
+func MonitorsSchemaEncoder(v reflect.Value) string {
+	var ids []string
+
+	m := v.Interface().([]models.Monitor)
+
+	for _, monitor := range m {
+		id := strconv.Itoa(monitor.Id)
+		ids = append(ids, id)
+	}
+
+	combined := strings.Join(ids, "-")
+	fmt.Println(combined)
+
+	return combined
 }
