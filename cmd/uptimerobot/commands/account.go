2e7dd6846a4d3e9e346343b8919d0946684ce2f2
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:35 -0500

    Use HTTP client for `account` subcommand

--- cmd/uptimerobot/commands/account.go
+++ cmd/uptimerobot/commands/account.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot/models"
+	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot"
 	"github.com/google/subcommands"
+	"os"
 )
 
 type Account struct {
@@ -23,11 +24,14 @@ func (p *Account) SetFlags(f *flag.FlagSet) {
 }
 
 func (p *Account) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	fmt.Println(models.Account{Email: "example@example.com",
-		DownMonitors: 2,
-		MonitorLimit: 50,
-		MonitorInterval: 5,
-		PausedMonitors: 1,
-		UpMonitors: 5})
+	client := uptime_robot.New()
+
+	account, err := client.GetAccountDetails()
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "Could not get account details")
+		return subcommands.ExitFailure
+	}
+
+	fmt.Println(account)
 	return subcommands.ExitSuccess
 }
