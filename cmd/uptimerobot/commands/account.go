2449e0802b4cb6b3e8962f6440b85a0cfa49d451
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:35 -0500

    Add mock account output

--- cmd/uptimerobot/commands/account.go
+++ cmd/uptimerobot/commands/account.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"context"
 	"flag"
+	"fmt"
+	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot/models"
 	"github.com/google/subcommands"
 )
 
@@ -21,5 +23,11 @@ func (p *Account) SetFlags(f *flag.FlagSet) {
 }
 
 func (p *Account) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	fmt.Println(models.Account{Email: "example@example.com",
+		DownMonitors: 2,
+		MonitorLimit: 50,
+		MonitorInterval: 5,
+		PausedMonitors: 1,
+		UpMonitors: 5})
 	return subcommands.ExitSuccess
 }
