fd9d131d35a210fa7039731be49cacba95f941c8
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:36 -0500

    encode OptionalNumber(0) as empty string

--- uptime_robot/breakcircle/optional_number.go
+++ uptime_robot/breakcircle/optional_number.go
@@ -2,7 +2,10 @@ package breakcircle
 
 // FIXME need to actually organize in a way that doesn't have circular imports
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strconv"
+)
 
 // OptionalNumber handles cases where the API uses an empty string instead of a null, or undefined
 // is used for an optional whole number value.
@@ -11,31 +14,39 @@ import "encoding/json"
 // unquoted. json.Number also supports floating point numbers which isn't useful for what these attributes represent.
 type OptionalNumber int64
 
-func (t *OptionalNumber) UnmarshalJSON(data []byte) error {
+func (n *OptionalNumber) UnmarshalJSON(data []byte) error {
 	// handle empty string
 	if len(data) == 2 && data[0] == '"' && data[len(data)-1] == '"' {
-		*t = OptionalNumber(0)
+		*n = OptionalNumber(0)
 		return nil
 	}
 
 	var numVal json.Number
 	err := json.Unmarshal(data, &numVal)
 	if err != nil {
-		*t = OptionalNumber(0)
+		*n = OptionalNumber(0)
 		return err
 	}
 
 	if len(numVal) == 0 {
-		*t = OptionalNumber(0)
+		*n = OptionalNumber(0)
 		return nil
 	}
 
 	i, err := numVal.Int64()
 	if err != nil {
-		*t = OptionalNumber(0)
+		*n = OptionalNumber(0)
 		return err // probably a decimal
 	}
 
-	*t = OptionalNumber(i)
+	*n = OptionalNumber(i)
 	return nil
+}
+
+func (n *OptionalNumber) String() string {
+	if *n == 0 {
+		return ""
+	}
+
+	return strconv.FormatInt(int64(*n), 10)
 }
