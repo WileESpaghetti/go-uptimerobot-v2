fd20860e099ebb7fd1baa565404cbdcdb63b09ba
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-31 23:47:35 -0500

    Get mock account details from the HTTP client

--- uptime_robot/client.go
+++ uptime_robot/client.go
@@ -1,6 +1,7 @@
 package uptime_robot
 
 import (
+	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot/models"
 	"net/http"
 	"strings"
 )
@@ -32,4 +33,13 @@ func NewRequest(apiMethod string) (*http.Request, error) {
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	return req, nil
+}
+
+func (c *Client) GetAccountDetails() (models.Account, error) {
+	return models.Account{Email: "example@example.com",
+		DownMonitors: 2,
+		MonitorLimit: 50,
+		MonitorInterval: 5,
+		PausedMonitors: 1,
+		UpMonitors: 5}, nil
 }
