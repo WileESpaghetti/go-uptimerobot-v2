cad291fd56521c2aa69e2c0cfe1969f70261a1f8
Lehman Black <WileESpaghetti@users.noreply.github.com>
2024-06-06 21:43:42 -0400

    clean up some of the account command

--- cmd/uptimerobot/cmd/account.go
+++ cmd/uptimerobot/cmd/account.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"github.com/WileESpaghetti/go-uptimerobot-v2/uptime_robot"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,41 +13,36 @@ import (
 // accountCmd represents the account command
 var accountCmd = &cobra.Command{
 	Use:   "account",
-	Short: "Display one or many resourcesPrint account information",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("account called")
-		if apiClient == nil { // FIXME this validation needs to be moved up a level so that it happens for all commands
-			_, _ = fmt.Fprintf(os.Stderr, "No API client found")
-			return
-		}
-
-		account, err := apiClient.GetAccountDetails()
-		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Could not get account details: %s\n", err)
-			return
-		}
-
-		fmt.Println(account)
-		return
+	Short: "Print information about your account",
+	Long: `The account command displays your user information, account limits, and number
+of up, down, and paused monitors.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return AccountAction(os.Stdout, apiClient)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(accountCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+var ErrNoApiClient = errors.New("no usable API client")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// accountCmd.PersistentFlags().String("foo", "", "A help for foo")
+const errGetAccountFailure = "could not get account details: %w"
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// accountCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func AccountAction(out io.Writer, urClient *uptime_robot.Client) error {
+	if urClient == nil {
+		return ErrNoApiClient
+	}
+
+	account, err := urClient.GetAccountDetails()
+	if err != nil {
+		return fmt.Errorf(errGetAccountFailure, err)
+	}
+
+	_, err = fmt.Fprintln(out, account)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
