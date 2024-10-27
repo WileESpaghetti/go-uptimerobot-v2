96d606841e4140aec2063e2edd0e7984af347379
Lehman Black <WileESpaghetti@users.noreply.github.com>
2024-10-27 16:54:36 -0400

    add None as an HttpAuthType Testing the API sometimes returned 0 or null and when I double-checked the web app they added None as a dropdown choice

--- uptime_robot/monitor/http_auth_type.go
+++ uptime_robot/monitor/http_auth_type.go
@@ -3,12 +3,15 @@ package monitor
 type HttpAuthType int64
 
 const (
+	HttpAuthTypeNone      HttpAuthType = 0
 	HttpAuthTypeHttpBasic HttpAuthType = 1
 	HttpAuthTypeDigest    HttpAuthType = 2
 )
 
 func (hat HttpAuthType) String() string {
 	switch hat {
+	case HttpAuthTypeNone:
+		return "None"
 	case HttpAuthTypeHttpBasic:
 		return "HTTP Basic Auth"
 	case HttpAuthTypeDigest:
