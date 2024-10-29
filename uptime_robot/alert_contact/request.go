b67c3afd8beddea0319fb93540a84150f4b05d0f
Lehman Black <WileESpaghetti@users.noreply.github.com>
2024-10-29 19:52:52 -0400

    add alert_contact.WithOptions

--- uptime_robot/alert_contact/request.go
+++ uptime_robot/alert_contact/request.go
@@ -14,6 +14,18 @@ type GetAlertContactsRequest struct {
 // AlertContactOptions configures a GetAlertContactsRequest
 type AlertContactOptions func(*GetAlertContactsRequest)
 
+// WithOptions allows setting all options for [getAlertContacts] request at once.
+//
+// This is a good option if you are setting many query parameters and do not
+// want to pass several AlertContactOptions.
+//
+// [getAlertContacts]: https://uptimerobot.com/api/
+func WithOptions(getAlertContacts *GetAlertContactsRequest) AlertContactOptions {
+	return func(options *GetAlertContactsRequest) {
+		*options = *getAlertContacts
+	}
+}
+
 // WithAlertContacts allows you to limit the data returned to specific alert contacts
 func WithAlertContacts(contacts AlertContacts) AlertContactOptions {
 	return func(options *GetAlertContactsRequest) {
